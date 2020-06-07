e092da2f85a444686ed7332bc3f1489407264bee
Vincent G <cho7rose@hotmail.com>
2020-06-07 19:40:07 +0100

    Added solution for Grains

---
+++ Grains/myCode/myCode.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace myCode
+{
+    public class Grains
+    {
+        public static ulong Square(int square)
+        {            
+            if (square<1 || square>64)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            return (ulong)Math.Pow(2,square-1);
+        }
+        public static ulong Total()
+        {
+            ulong t=0;
+            for(int i=1;i<65;i++)
+            {
+                t+=Grains.Square(i);
+            }
+            return (ulong)t;
+        }
+    }
+}
