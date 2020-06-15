7abe4e440764a696b1443580dce9ec8c9e245afd
Vincent G <cho7rose@hotmail.com>
2020-06-15 16:39:32 +0100

    Added All Your Base

---
+++ All Your Base/myCode/myCode.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace myCode
+{
+    public class AllYourBase
+    {
+        public static int[] Rebase(int iBase, int[] theirDigits, int oBase)
+        {
+            if(iBase<2||oBase<2){throw new ArgumentException();}
+            var myDigits = new List<double>();
+            int temp=0;
+            var max=theirDigits.Count();
+                for(int i=0; i<max; i++)
+                {
+                    if(theirDigits[i]<0||theirDigits[i]>iBase-1){throw new ArgumentException();}
+                    temp+=(int)(theirDigits[i]*Math.Pow(iBase,max-i-1));
+                }
+                while(temp>0)
+                {
+                    myDigits.Add(temp%oBase);
+                    temp=temp/oBase;
+                }
+            myDigits.Reverse();
+            int[] res;
+            if(myDigits.Sum()==0)
+            {
+                res=new[] {0};
+            }
+            else
+            {
+                res=new int[myDigits.Count()];
+                int j=0;
+                foreach(int digit in myDigits)
+                {
+                    res[j]=digit;
+                    j++;
+                }
+            }
+            return res;
+        }
+    }
+}
